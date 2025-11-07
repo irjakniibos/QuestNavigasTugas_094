6e9042a04e315164df2df5b165f0da82119a9ed3
irjakniibos <irza.yaumil.ft23@mail.umy.ac.id>
2025-11-07 13:56:01 +0700

    Menambahkan Function daftarData

--- app/src/main/java/com/example/questnavigastugas_094/view/TampilData.kt
+++ app/src/main/java/com/example/questnavigastugas_094/view/TampilData.kt
@@ -1,2 +1,11 @@
 package com.example.questnavigastugas_094.view
 
+import androidx.compose.runtime.Composable
+
+@Composable
+fun DaftarData(
+    onTambahDataClicked:() -> Unit,
+    onBerandaClicked:() -> Unit
+){
+
+}
