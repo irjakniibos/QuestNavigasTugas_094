cddf3299bdabaafeab8c6e856d3e6ddbb110b50c
irjakniibos <irza.yaumil.ft23@mail.umy.ac.id>
2025-11-07 14:12:46 +0700

    Menambahkan Function Formulir Pendaftaran

--- app/src/main/java/com/example/questnavigastugas_094/view/FormPendaftaran.kt
+++ app/src/main/java/com/example/questnavigastugas_094/view/FormPendaftaran.kt
@@ -1,2 +1,12 @@
 package com.example.questnavigastugas_094.view
 
+import androidx.compose.runtime.Composable
+import java.lang.reflect.Modifier
+
+@Composable
+fun FormulirPendaftaran(
+    onSubmitButtonClicked: () -> Unit,
+    modifier: Modifier = Modifier
+){
+
+}
