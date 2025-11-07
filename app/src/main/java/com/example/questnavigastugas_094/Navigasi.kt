da71922f5444ecdb2c31f93d9e56c4eb3aea5a92
irjakniibos <irza.yaumil.ft23@mail.umy.ac.id>
2025-11-07 14:35:04 +0700

    menambahkan Function AplikasiPendaftaran

--- app/src/main/java/com/example/questnavigastugas_094/Navigasi.kt
+++ app/src/main/java/com/example/questnavigastugas_094/Navigasi.kt
@@ -1,8 +1,18 @@
 package com.example.questnavigastugas_094
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+
 enum class{
     SelamatDatang,
     DaftarPeserta,
     Formulir
 }
 
+@Composable
+fun AplikasiPendaftaran(
+    navController: NavHostController = rememberNavController()
+){
+
+}
