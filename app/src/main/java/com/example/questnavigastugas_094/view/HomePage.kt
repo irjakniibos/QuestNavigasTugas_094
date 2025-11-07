3b6016bd6bb24b472261d97cdd216f1e5b25e65a
irjakniibos <irza.yaumil.ft23@mail.umy.ac.id>
2025-11-07 13:49:35 +0700

    Menambahkan Text Selamat Datang dan mengatur style mulai dari font family dan spacer

--- app/src/main/java/com/example/questnavigastugas_094/view/HomePage.kt
+++ app/src/main/java/com/example/questnavigastugas_094/view/HomePage.kt
@@ -2,11 +2,16 @@ package com.example.questnavigastugas_094.view
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -20,3 +25,11 @@ fun HalamanUtama(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
         )
+    {
+        Text("Selamat Datang", style = MaterialTheme.typography.headlineLarge, fontFamily = FontFamily.Monospace)
+        Spacer(modifier = Modifier.height(32.dp))
+
+
+
+    }
+}
